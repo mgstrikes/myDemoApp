ee735f9eb3f4ea5d0774dcff8253c0ced3c21f68
Manu George <30804841+mgstrikes@users.noreply.github.com>
2018-10-16 06:28:52 +0530

    Message changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'the world of Angular 5';
+  title = 'the world of Angular 5 with mgstrikes';
 }
